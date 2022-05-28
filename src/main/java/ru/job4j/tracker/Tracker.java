172044c798ede6cbcfd6ed1d83ce4c97b4cbb769
alisa <12345678>
2022-05-28 10:22:43 +0400

    fix 5. Tracker - хранилище [#396 #306030]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,17 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
-            if (item.getName() != null) {
-                rsl[count] = items[i];
-                count++;
-            }
-        }
-        rsl = Arrays.copyOf(rsl, count);
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
