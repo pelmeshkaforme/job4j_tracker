638b99ed41fd5693aa0c1969b89460ef4cebdfdd
alisa <12345678>
2022-05-30 18:16:38 +0400

    2.2. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,16 +1,37 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
-    public static void main(String[] args) {
-        Item item = new Item();
-        LocalDateTime rsl = item.getCreated();
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String rslFormat = rsl.format(formatter);
-        System.out.println("Вывод на консоль - " + rslFormat);
-        System.out.println(item);
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.println("Введите номер пункта меню");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь ввел - " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item", "Delete item", "Find item by id",
+                "Find items by name", "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + "." + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        Scanner scanner = new Scanner(System.in);
+        new StartUI().init(scanner, tracker);
     }
 }
