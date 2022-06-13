8f00bbc025149e2930e5a03c836073e5be748bc6
alisa <12345678>
2022-06-13 12:09:53 +0400

    9.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/EditItemAction.java
+++ src/main/java/ru/job4j/tracker/EditItemAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class EditItemAction implements UserAction {
+    private final Output out;
+
+    public EditItemAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Edit Item";
--- src/main/java/ru/job4j/tracker/FindAllItemAction.java
+++ src/main/java/ru/job4j/tracker/FindAllItemAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class FindAllItemAction implements UserAction {
+    private final Output out;
+
+    public FindAllItemAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Find all Item";
