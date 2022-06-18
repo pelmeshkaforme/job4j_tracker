b739820d53c359ead5a926a85b12272b43cf910d
alisa <12345678>
2022-06-18 17:37:43 +0400

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -340,4 +340,33 @@ public class StartUITest {
                         + "1.Exit" + ln
         ));
     }
+
+    @Test
+    public void whenInvalidEnterMenuNumber() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Input in = new StubInput(
+                new String[] {"3", "0", "New item name", "1"}
+        );
+        UserAction[] actions = {
+                new CreateAction(output),
+                new ExitAction()
+        };
+        new StartUI(output).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString(), is(
+                "Menu:" + ln
+                + "0.Add new Item" + ln
+                + "1.Exit" + ln
+                + "Wrong input, you can select: from 0 to " + (actions.length - 1) + ln
+                + "Menu:" + ln
+                + "0.Add new Item" + ln
+                + "1.Exit" + ln
+                + "=====Add item menu=====" + ln
+                + "Добавлена заявка: " + tracker.findAll()[0] + ln
+                + "Menu:" + ln
+                + "0.Add new Item" + ln
+                + "1.Exit" + ln
+        ));
+    }
 }
